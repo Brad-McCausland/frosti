ca5b37e5c240c2906517188884dd05f12351957e
tr3mulant <scottkstjohn87@gmail.com>
2016-05-14 16:11:32 -0700

    added freezer.py and modded read.py to use the new freezer object and return the average temperature for a given freezer

---
+++ freezer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Scott St. John
+# 5/14/16 
+# CSCI 492: Senior Project
+# freezer.py
+#
+
+# Simple data container object
+
+from thermistor import Thermistor
+
+class Freezer (object):
+
+        def __init__(self, iD):
+            self.iD = iD
+            self.thermistorList = []
+
