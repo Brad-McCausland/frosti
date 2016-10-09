aab70abb4f0f481960a5cca16210515ab4d4b2a4
Avery Pierce <fredknex@gmail.com>
2016-10-08 20:05:54 -0700

    support for addphone.py and addemail.py
    form data sent to scripts, redirected to a page with result

--- flask/hello.py
+++ flask/hello.py
@@ -3,26 +3,59 @@ import flask
 app=flask.Flask(__name__)
 
 import os
+#import sys
 import time
 import subprocess
 
+#path = sys.path[0] #sys.path[0] is directory script was launched from
+
+#scipt is a string "script.py"
+def runScript(script,arg1,arg2):
+    path = os.path.join(os.path.expanduser('~'),"frostiSrc/scripts/" + script)
+    process = subprocess.Popen([arg1,arg2],stdout=subprocess.PIPE)
+    result = process.stdout.read()
+    return result
+
 def readUserData(type):
     list = []
-    file = open(os.path.join(os.path.expanduser('~'),"frostiSrc/alertSrc/user_register/" + type + ".txt"), 'r')
+    file = open(os.path.join(os.path.expanduser('~'),"frostiSrc/alertSrc/user_register/" + type), 'r')
     for line in file:
         if line != '\n':
             list.append(line.split(' ')[0])
     return list
 
-mailinglist = readUserData("email")
-print(mailinglist)
-phonenumbers = readUserData("phone")
+mailinglist = readUserData("email.txt")
+phonenumbers = readUserData("phone.txt")
+
+
+replaceThis = "replaceThis"
 
 @app.route('/addemail', methods=['POST'])
 def login():
     if request.method == 'POST':
-        print(request.form['newemail']) #subprocess here
-        return redirect(url_for('hello'))
+        arg1 = request.form['newemail']
+        arg2 = request.form['scope']
+        result = runScript("addemail.py",arg1,arg2)
+
+        replaceThis = result
+
+        return redirect(url_for('formresult')) #'url_for' wants def name(), not @app.route
+
+@app.route('/addphone', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        arg1 = request.form['newphone']
+        arg2 = request.form['scope']
+        result = runScript("addphone.py",arg1,arg2)
+
+        replaceThis = result
+
+        return redirect(url_for('formresult')) #'url_for' wants def name(), not @app.route
+
+#just returning a string right now but this shold be set up as a real page with a template
+@app.route("/formresult")
+def formresult():
+    return replaceThis
 
 @app.route("/")
 def hello():
--- scripts/addemail.py
+++ scripts/addemail.py
@@ -15,18 +15,18 @@ def main():
 	scope = scope.lower()
 
 	if scope != "all" and scope != "freezer" and scope != "vivarium":
-		print "Error: scope '"+scope+"' not recognized (accepted values are 'all', 'freezer', and 'vivarium')"
+		print ("Error: scope '"+scope+"' not recognized (accepted values are 'all', 'freezer', and 'vivarium')")
 		return
 
 	emailFile = open(os.path.join(os.path.expanduser('~'),"frostiSrc/alertSrc/user_register/email.txt"), 'a+')
 
 	for line in emailFile:
 		if address in line:
-			print "Error: This email address is already registered in our email directory!"
+			print ("Error: This email address is already registered in our email directory!")
 			return
 
 	emailFile.write(address+" "+scope+"\n")
 
-	print address+" will now be sent "+scope+" messages!"
+	print (address+" will now be sent "+scope+" messages!")
 
 main()
