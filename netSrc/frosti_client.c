f63a5d0c2fe7c0440d75f73d42e38d5e3ddec41c
bmcc0605 <mccausland0605@gmail.com>
2016-10-10 14:10:12 -0700

    Fixed off-by-one err in displaying month in logs

--- netSrc/frosti_client.c
+++ netSrc/frosti_client.c
@@ -43,7 +43,7 @@ main( int argc, char **argv) {
         time_t t = time(NULL);
         struct tm tm = *localtime(&t);
 
-        fprintf(logfile, "%d/%d/%d %d:%d:%d - ", tm.tm_year + 1900, tm.tm_mon, tm.tm_mday,
+        fprintf(logfile, "%d/%d/%d %d:%d:%d - ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
                                                  tm.tm_hour, tm.tm_min, tm.tm_sec);
 
 	memset((char *)&sad,0,sizeof(sad)); /* clear ip addr structure */
