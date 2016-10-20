0ab242a65cb84e75b132a137607739f771c67dfa
Bradley McCausland <mccausb@LabImage2016-17>
2016-10-20 15:05:38 -0700

    Added twilio keep-alive messages

---
+++ alertSrc/twilioKeepAlive.py
@@ -0,0 +1,11 @@
+# This is a simple script to be run twice a month
+# with the purpose of keeping twilio from revoking
+# frosti's number (twilio will cancel a number if
+# it goes more than a month without being used).
+#
+# This sends a simple message to users ostensibly
+# as a test.
+
+import alert
+
+alert.send("This is a test of the FROSTI alert system. There is no emergency at this time.", "all")
