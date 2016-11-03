fffc8b44f0d596bf227ec06a8bba51b08a0e937b
Avery Pierce <fredknex@gmail.com>
2016-11-02 19:14:00 -0700

    user login/logout working

--- flask/hello.py
+++ flask/hello.py
@@ -1,18 +1,74 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, flash, request, render_template, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 import flask
-app=flask.Flask(__name__)
 
 import os
 #import sys
 import time
 import subprocess
 
+
+app=flask.Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "login"
+
+class User(UserMixin):
+    user_database = {"Admin": ("Admin", "adminpassword"),
+               "Admin2": ("Admin2", "megagoodpassword")}
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+
+    @classmethod
+    def get(cls,id):
+        return cls.user_database.get(id)
+
+#this thing needs to find user from db, create instance of and return it.
+@login_manager.user_loader
+def load_user(id):
+    user = User.get(id)
+    return User(user[0],user[1])
+
 #path = sys.path[0] #sys.path[0] is directory script was launched from
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    #This checks for the username in the 'database'.
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    username = request.form['username']
+    password = request.form['password']
+    rememberme = False
+    if 'rememberme' in request.form:
+        rememberme = True
+
+    dbResult = User.get(username)
+    if dbResult is not None:
+    #Yep, so we send login_user a user, AND the user_loader thing also makes one.
+        user = User(dbResult[0],dbResult[1])
+        if password == user.password:
+            login_user(user, remember = rememberme)
+            flash('Login Successful')
+            next = flask.request.args.get('next')
+
+            return flask.redirect(flask.url_for('hello'))
+
+    flash('Login Failed')
+    return flask.redirect(flask.url_for('login'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 #scipt is a string "script.py"
 def runScript(script,args):
 
-    path = os.path.join(os.path.expanduser('~'),"frosti/scripts/" + script)
+    path = os.path.join(os.path.expanduser('~'),"frostiSrc/scripts/" + script)
     command = ["python3",path]
     for each in args:
         command.append(each)
@@ -25,7 +81,7 @@ def runScript(script,args):
 #other one is email
 def readUserData(type):
     list = []
-    file = open(os.path.join(os.path.expanduser('~'),"frosti/alertSrc/user_register/" + type), 'r')
+    file = open(os.path.join(os.path.expanduser('~'),"frostiSrc/alertSrc/user_register/" + type), 'r')
     for line in file:
         if line != '\n':
             list.append(line.split(' ')[0])
@@ -96,9 +152,15 @@ def deletephone():
 def formresult():
     return render_template('result.html',scriptResult=request.args.get('scriptResult'))
 
+@app.route("/templogin")
+def templogin():
+    return render_template('login.html')
+
 @app.route("/")
+@login_required
 def hello():
     return render_template('index.html',mailinglist=mailinglist,phonenumbers=phonenumbers)
 
 if __name__ == "__main__":
+    app.config["SECRET_KEY"] = '%hwgS\x13\x97\xf4\xee\xf8\xa8< \x11\xd1*\xe8\xecV\x95\xfbeY\xbe'
     app.run(host='0.0.0.0',port=5000)
