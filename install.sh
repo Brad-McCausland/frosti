2c51e5a90ba97c7b1500de9ecbcfe3606852c2aa
bmcc0605 <mccausland0605@gmail.com>
2016-11-02 20:55:08 -0700

    Installer now configures email

--- install.sh
+++ install.sh
@@ -16,3 +16,31 @@ update-rc.d startFrostiServer.sh defaults
 
 #set timezone
 sudo echo "America/Vancouver" > /etc/timezone
+
+#configure ssmtp
+sudo echo "#" > /etc/ssmtp/ssmtp.conf
+sudo echo "# Config file for sSMTP sendmail" >> /etc/ssmtp/ssmtp.conf
+sudo echo "#" >> /etc/ssmtp/ssmtp.conf
+sudo echo "# The person who gets all mail for userids < 1000" >> /etc/ssmtp/ssmtp.conf
+sudo echo "# Make this empty to disable rewriting." >> /etc/ssmtp/ssmtp.conf
+sudo echo "root=frosti.wwu@gmail.com" >> /etc/ssmtp/ssmtp.conf
+
+sudo echo "# The place where the mail goes. The actual machine name is required no " >> /etc/ssmtp/ssmtp.conf
+sudo echo "# MX records are consulted. Commonly mailhosts are named mail.domain.com" >> /etc/ssmtp/ssmtp.conf
+sudo echo "mailhub=smtp.gmail.com:587" >> /etc/ssmtp/ssmtp.conf
+
+sudo echo "# Where will the mail seem to come from?" >> /etc/ssmtp/ssmtp.conf
+sudo echo "rewriteDomain=" >> /etc/ssmtp/ssmtp.conf
+
+sudo echo "# The full hostname" >> /etc/ssmtp/ssmtp.conf
+sudo echo "hostname=raspberrypi" >> /etc/ssmtp/ssmtp.conf
+
+sudo echo "# Are users allowed to set their own From: address?" >> /etc/ssmtp/ssmtp.conf
+sudo echo " YES - Allow the user to specify their own From: address" >> /etc/ssmtp/ssmtp.conf
+sudo echo " NO - Use the system generated From: address" >> /etc/ssmtp/ssmtp.conf
+sudo echo "FromLineOverride=YES" >> /etc/ssmtp/ssmtp.conf
+sudo echo "AuthUser=frosti.wwu@gmail.com" >> /etc/ssmtp/ssmtp.conf
+sudo echo "AuthPass=frosti492" >> /etc/ssmtp/ssmtp.conf
+sudo echo "UseTLS=Yes" >> /etc/ssmtp/ssmtp.conf
+sudo echo "UseSTARTTLS=Yes" >> /etc/ssmtp/ssmtp.conf
+sudo echo "AuthLogin=Yes" >> /etc/ssmtp/ssmtp.conf
