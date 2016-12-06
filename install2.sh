e6019706a7dde5263b969129b095abda27819857
Brad McCausland <mccausb@students.wwu.edu>
2016-12-06 11:36:35 -0800

    Added sync_contacts to installer. Re-wrote to take command line input instead of args

--- install2.sh
+++ install2.sh
@@ -4,34 +4,33 @@
 
 #set cron jobs
 
-while [[ $# -gt 1 ]]
-do
-key="$1"
+echo "Will this be the active pi (will it be running the webserver?) y/n"
 
-case $key in
-    -a|--active)
-    ACTIVE="$2"
-    shift # past argument
-    ;;
-    *)
-            # unknown option
-    ;;
-esac
-shift # past argument or value
-done
+read ACTIVE
 
 cd ~
 crontab -l > mycron
-if [ $ACTIVE == "yes" -o $ACTIVE == "y" ]
+if [ $ACTIVE == "y" ]
 then
 	echo "*/5 * * * * python /home/pi/frosti/frosti_driver.py" >> mycron
-else
+	echo "0 12 1,15 * * python /home/pi/frosti/alertSrc/twilioKeepAlive.py" >> mycron
+elif [$ACTIVE == "n"]
+then
+	# run inactive driver (subject to change)
 	echo "*/5 * * * * python /home/pi/frosti/testDriver.py" >> mycron
+
+	# sync contact info with active pi
+	echo "0 0 * * * /home/pi/frosti/alertSrc/sync_contacts" >> mycron
+
+	# remove duplicate web server
+	sudo rm /etc/init.d/startFrostiWeb.sh
+else
+	echo "Please enter either y or n. Aborting installer."
+	exit 1
 fi
+
 echo "1-59/15 7-19 * * * /home/pi/frosti/netSrc/frosti_client 127.0.0.1 8001" >> mycron
 
-#The idea here is to send a message at noon on the 1st and 15th of each month. Might be bad syntax
-echo "0 12 1,15 * * python /home/pi/frosti/alertSrc/twilioKeepAlive.py" >> mycron
 crontab mycron
 rm mycron
 
