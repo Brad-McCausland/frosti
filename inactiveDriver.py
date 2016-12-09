4d0ee0e6e61ac0fc829004d8037b899d3e11eee2
Brad McCausland <mccausb@students.wwu.edu>
2016-12-08 22:09:39 -0800

    Removing backup files, an old todo. Updated comments on inactive driver saying it should no longer be used

--- inactiveDriver.py
+++ inactiveDriver.py
@@ -9,12 +9,17 @@
 # feasible I've decided to not change the design in the hope
 # that one day the two active pi design will be implemented.
 #
+# Addendum: Huzzah, both Pis can read temps now. This will
+# stay in the repo because it's good for testing if you
+# don't have access to the lab, but this should no longer
+# be necessary on the production system
+#
 # This version operates the same as the active driver, but
 # instead of reading from thermistors it just injects '-80.0'
 # for each thermistor in all read operations.
 
-from readSrc.temp import *
-from readSrc.read import *
+#from readSrc.temp import *
+#from readSrc.read import *
 from loggerSrc.logger import *
 from alertSrc.alert import *
 
@@ -27,18 +32,18 @@ import datetime
 active = True
 
 #directory of log files
-logDir = "/home/pi/frosti/logs/"
+logDir = "/home/bmcc0605/frosti/logs/"
 
 def run():
-    read.gpio_init()
-    freezers_init()
-    thermistors_init()         
+    #read.gpio_init()
+    #freezers_init()
+    #thermistors_init()         
     tempList = []
     for freezer in range (0,3):
         time = datetime.datetime.now()
-        temperature = -80.0 #only difference
+        temperature = -80.0 #insert test value instead of calc_temp(freezer)
         tempList.append(temperature)
-        if temperature > -70.0:
+        if temperature > -60.0:
             if active: #moved active check here to ensure logging happens on inactive pi
                 #need to sleep and retake temp
                 error = "Warning: Freezer " + str(freezer) + " has reached " + str('%.1f'%(temperature)) + "*C at " + str(time)[10:19] + "."
@@ -55,5 +60,23 @@ def run():
     n = log(logDir, tempList[0], tempList[1], tempList[2])
     if n == -1:
         print("error")
+    #check_partner(other)
+
+def check_partner(other):
+    #ping other
+    if fail:
+        #ping again, break if success
+        active = true
+        error = "Warning: Pi " + str(other) + " is down. Please replace soon."
+        n = send(error, "all")
+        if n == -1:
+            #email error
+            print("Email failed to send.")
+        elif n == -2:
+            #text error
+            print("SMS failed to send.")
+        elif n == -3:
+            #email and text error
+            print("Email and SMS failed to send.")
     
 run()
